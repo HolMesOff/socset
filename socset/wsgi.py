a464d5a8ecaecca2a4cae0329edae78d611db182
HolMesOff <113038057+HolMesOff@users.noreply.github.com>
2024-12-02 14:07:55 +0600

    good

--- socset/wsgi.py
+++ socset/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for socset project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socset.settings')
 
+# Render-specific: Используем порт из переменной окружения
+port = os.getenv('PORT', 8000)
 application = get_wsgi_application()
